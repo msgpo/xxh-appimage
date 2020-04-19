91b8b85d22ca30ad5763f8699e1dcd503e34df9a
anki-code <anki-code>
2020-04-19 11:22:31 +0300

    build

--- appimage/recipes/xonsh-release.sh
+++ appimage/recipes/xonsh-release.sh
@@ -1,3 +1,3 @@
 export PYTHON_VERSION="3.8.2"
-export PIP_REQUIREMENTS="xonsh==0.9.14 prompt_toolkit==3.0.3 gnureadline Pygments"
+export PIP_REQUIREMENTS="xonsh==0.9.13 prompt_toolkit gnureadline Pygments"
 export PYTHON_ENTRYPOINT='"-u" "-c" "from xonsh.main import main; main()"'
